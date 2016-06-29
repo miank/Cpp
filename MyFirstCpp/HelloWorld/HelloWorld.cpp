59ce4e9fd4cbd0811b7e2e041d7dd8cf19198798
Miank <ankit1703@gmail.com>
2016-06-29 07:36:10 +0530

    Initial Commit
    Hello World Code Snippet Welcome to C++ !!!

---
+++ MyFirstCpp/HelloWorld/HelloWorld.cpp
@@ -0,0 +1,15 @@
+// HelloWorld.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	cout << "Hello World";
+	cout << endl;
+	return 0;
+}
+
