1f7c780e789857e105e50ae32b4e31425fddabb8
Miank <ankit1703@gmail.com>
2016-06-30 06:07:23 +0530

    Added double array

--- ArrayDemo/MyArray/MyArray.cpp
+++ ArrayDemo/MyArray/MyArray.cpp
@@ -18,6 +18,14 @@ int main()
 	cout << "The lowest prime number is: " << m_aPrime[0] << "\n";
 	cout << "The Sum of the first 5 primes is: " << m_aPrime[0] + m_aPrime[1] + m_aPrime[2] + m_aPrime[3] + m_aPrime[4] << "\n";
 
+	// Array of type double
+	double m_aMyArray[3];  // Allocate 3 variables
+	m_aMyArray[0] = 2.0;
+	m_aMyArray[1] = 3.0;
+	m_aMyArray[2] = 4.3;
+
+	cout << "The average is of m_aMyArray " << (m_aMyArray[0] + m_aMyArray[1] + m_aMyArray[2]) / 3 << endl;
+	
 	return 0;
 }
 
