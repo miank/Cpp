68374a8dfa7936d2c003888ae3a19bf882d0808a
Miank <ankit1703@gmail.com>
2016-06-30 06:33:06 +0530

    Added element to array.

--- ArrayDemo/MyArray/MyArray.cpp
+++ ArrayDemo/MyArray/MyArray.cpp
@@ -8,15 +8,16 @@ using namespace std;
 
 int main()
 {
-	int m_aPrime[5];  // hold the first 5 elements
+	short m_aPrime[6];  // hold the first 5 elements
 	m_aPrime[0] = 2;
 	m_aPrime[1] = 3;
 	m_aPrime[2] = 5;
 	m_aPrime[3] = 7;
-	m_aPrime[4] = 11;
+	m_aPrime[4] = 11; 
 	
 	cout << "The lowest prime number is: " << m_aPrime[0] << "\n";
 	cout << "The Sum of the first 5 primes is: " << m_aPrime[0] + m_aPrime[1] + m_aPrime[2] + m_aPrime[3] + m_aPrime[4] << "\n";
+	cout << "The array element which are not initialized, they are default set to 0" << m_aPrime[5] << endl;
 
 	// Array of type double
 	double m_aMyArray[3];  // Allocate 3 variables
