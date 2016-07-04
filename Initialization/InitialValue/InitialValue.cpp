815f10b88645a73b5b8e1dabb650288e5e979082
Miank <ankit1703@gmail.com>
2016-07-04 07:20:03 +0530

    Initial Commit
    Initialization.

---
+++ Initialization/InitialValue/InitialValue.cpp
@@ -0,0 +1,19 @@
+// InitialValue.cpp : Initial value of differnet data types.
+// If you build in release configuration then will print random address every time when you build.
+// If you build in debug mode it will crash the application with Run Time Error #3
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	// define a integer variable named x
+	int x; 
+
+	// print the value of x 
+	cout << x << endl;
+	return 0;
+}
+
