1233ddf293763faf95b6cba268ec1b7a9e83af88
Miank <ankit1703@gmail.com>
2016-08-07 18:22:41 +0530

    initial commit

---
+++ InitializationInCpp/Initialization/Initialization.cpp
@@ -0,0 +1,22 @@
+// Initialization.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+
+class class_a {
+public:
+	class_a() {}
+	class_a(String str) : m_string{ str } {}
+
+	double m_dString;
+	string m_string;
+
+};
+int main()
+{
+
+    return 0;
+}
+
