6145181dea6cdd06b34f24978ce5ba9e24e3e042
Ankit Mishra <ankit1703@gmail.com>
2016-09-12 13:09:17 +0530

    Initail Commit

---
+++ Inheritance/Counter/Counter.cpp
@@ -0,0 +1,41 @@
+// Counter.cpp : Inheritance with counter class
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+class Counter
+{
+protected:
+	unsigned int count;
+public:
+	Counter() : count(0){ }
+	Counter(int c) : count(c) { }
+	unsigned int get_count() const { return count; } // return count
+	Counter operator++() 
+	{
+		return Counter(++count);
+	}
+};
+
+class CountDn : public Counter {
+public:
+	Counter operator--()
+	{
+		return Counter(--count);
+	}
+};
+
+int main()
+{
+	CountDn c1;
+	cout << "\n c1 = " << c1.get_count();
+	++c1; ++c1; ++c1;
+	cout << "\n c1 = " << c1.get_count();
+	cout << endl;
+	--c1; --c1;
+	cout << "\n c1 = " << c1.get_count();
+	cout << endl;
+    return 0;
+}
+
