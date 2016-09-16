f85672b91cf1626380ce42b5d20645158f852f7d
Ankit Mishra <ankit1703@gmail.com>
2016-09-16 11:48:28 +0530

    Initial Commit

---
+++ UninitializedVariable/Variables/Variables.cpp
@@ -0,0 +1,23 @@
+// Variables.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int x(0);
+	float f(0.0);
+	double d(0);
+	long l(0L);
+	char c = 'a';
+	cout << "int value is " << x << endl;
+	cout << "float value is " << f << endl;
+	cout << "double value is " << d << endl;
+	cout << "long value is " << l << endl;
+	cout << "char value is " << c << endl;
+    return 0;
+}
+
