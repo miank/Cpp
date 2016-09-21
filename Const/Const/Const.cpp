61fb0501a74d748b625a5de46cf513fbfef74642
Ankit Mishra <ankit1703@gmail.com>
2016-09-21 07:35:06 +0530

    INitial Commit

---
+++ Const/Const/Const.cpp
@@ -0,0 +1,26 @@
+// Const.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+// Comst is useful in function
+void printInteger(const int myValue){
+	cout << myValue << endl;
+}
+
+int main()
+{
+	const int x{ 4 };
+	// x = 5; // ERROR
+	const double gravity{ 9.8 }; // Add const before type
+
+	cout << "Enter your age " << endl;
+	int age;
+	cin >> age;
+	const int usersAge(age);
+
+    return 0;
+}
+
