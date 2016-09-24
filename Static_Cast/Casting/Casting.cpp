eade9c83b962cd34d5f58218f5bc320fb5ebc5c5
Ankit Mishra <ankit1703@gmail.com>
2016-09-24 13:31:14 +0530

    Initial Commit

---
+++ Static_Cast/Casting/Casting.cpp
@@ -0,0 +1,20 @@
+// Casting.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int x = 7;
+	int y = 4;
+	cout << "int / int = " << x / y << "\n";
+	cout << "double / int = " << static_cast<double>(x) / y << "\n";
+	cout << "int / double = " << x / static_cast<double>(y) << "\n";
+	cout << "int / double = " << x / static_cast<double>(y) << "\n";
+	cout << "double / double = " << static_cast<double>(x) / static_cast<double>(y) << "\n";
+    return 0;
+}
+
