9b42bce0d19a67966f19e6b33c0fb29f804281d1
Miank1703 <toankitmishra@gmail.com>
2017-01-20 11:52:49 +0530

    Final Commit

---
+++ AutoKeyword/Auto/Auto.cpp
@@ -0,0 +1,24 @@
+// Auto.cpp : Prior to C++11, the auto keyword was probably the least used keyword in C++. 
+// Local variables and local scope, you learned that local variables have automatic duration
+// (they’re created at the point of definition, and destroyed when the block they are part of is exited).
+
+// Starting with C++11, the auto keyword does just that. When initializing a variable, 
+// the auto keyword can be used in place of the variable type to tell the compiler to infer the 
+// variable’s type from the assignment’s type. This is called automatic type deduction.
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+int add(int a, int b)
+{
+	return a + b;
+}
+int main()
+{
+	auto sum = add(5, 6);
+	cout << sum << endl;
+
+    return 0;
+}
+
