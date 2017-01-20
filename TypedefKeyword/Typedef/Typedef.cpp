3079cc10147342ca1749887db7983f54a99ff354
Miank1703 <toankitmishra@gmail.com>
2017-01-20 12:08:21 +0530

    Initial Commit

---
+++ TypedefKeyword/Typedef/Typedef.cpp
@@ -0,0 +1,15 @@
+// Typedef.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	std::int8_t i(97);
+	cout << i << endl;
+
+    return 0;
+}
+
