294debd0c07402cec0085841d223f90d6f237236
Miank1703 <toankitmishra@gmail.com>
2017-01-21 14:45:48 +0530

    Initial Commit

---
+++ STL/List/List.cpp
@@ -0,0 +1,49 @@
+// List.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <list>
+#include <string>
+using namespace std;
+
+// function for printing the list
+void showlist(list<int> g)
+{
+	list<int>::iterator it;
+	for (it = g.begin(); it != g.end(); it++)
+	{
+		cout << "\t " << *it;
+	}
+	cout << endl;
+}
+
+
+int main()
+{
+	list<int> list1, list2;
+	for (int i = 0; i < 10; i++)
+	{
+		list1.push_back(i * 2);
+		list2.push_front(i * 3);
+	}
+	cout << "List1 is : " << endl;
+	showlist(list1);
+	cout << "List2 is : " << endl;
+	showlist(list2);
+
+	cout << "List front " << list1.front() << endl;
+	cout << "List end " << list1.back() << endl;
+	cout << "List1 Pop front ";
+	list1.pop_front();
+	showlist(list1);
+	cout << "List2 Pop front ";
+	list2.pop_front();
+	showlist(list2);
+	cout << "Reverse the list " << endl;
+	list1.reverse();
+	showlist(list1);
+	showlist(list2);
+    return 0;
+}
+
