059b2923e967a5a71733c69806dcd45fcdaace4b
miank <ankit1703@gmail.com>
2017-01-24 22:47:58 +0530

    Initial Commit

---
+++ STL/Set/Set.cpp
@@ -0,0 +1,23 @@
+// Set.cpp : 
+// std::set internally store elements in balanced binary tree.
+// It’s doesn’t allow duplicate elements i.e. it only contains unique elements.
+// 
+
+#include "stdafx.h"
+#include <iostream>
+#include <set>
+using namespace std;
+
+class Sample
+{
+
+};
+
+int main()
+{
+	set<int> s1;
+	set<Sample> s2;
+
+    return 0;
+}
+
