63b62d37ef3b0929c895cbc44987b846bbd6992a
miank <ankit1703@gmail.com>
2017-02-02 13:33:26 +0530

    Some changes added.

--- NamespaceDemo/Namespace/Namespace.cpp
+++ NamespaceDemo/Namespace/Namespace.cpp
@@ -13,13 +13,53 @@ namespace first
 // Global Variable
 int val = 100;
 
+namespace ns1
+{
+	int value()
+	{
+		return 5;
+	}
+}
 
+namespace ns2
+{
+	const double x = 100;
+	double value()
+	{
+		return 2 * x;
+	}
+}
+
+// class and namespace
+namespace ns3
+{
+	void display();
+	class Test
+	{
+	public:
+		void Display()
+		{
+			cout << "ns3::Test::Display() " << endl;
+		}
+	};
+}
+
+void ns3::display()
+{
+	cout << "Display " << endl;
+}
 
 
 int main()
 {
 	int val = 200;
 	cout << first::val << endl;
+	cout << ns1::value() << endl;
+	cout << ns2::value() << endl;
+	cout << ns2::x << endl;
+	ns3::Test Obj;
+	Obj.Display();
+	ns3::display();
 	return 0;
 }
 
