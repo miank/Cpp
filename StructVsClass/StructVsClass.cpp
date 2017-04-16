7e1b8c587d9c7c3ef70b9d352cd7a25ca9f741f6
Ankit Mishra <ankit1703@gmail.com>
2017-04-16 11:44:15 +0530

    Initial Commit

---
+++ StructVsClass/StructVsClass.cpp
@@ -0,0 +1,49 @@
+// StructVsClass.cpp 
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+// data conatiner and carry public data
+struct Person_t
+{
+	// All data members are public
+	string name;
+	unsigned age = 20;
+private:
+	int height;
+};
+
+// carry private data and used for more complex data structure like member function
+
+class Person
+{
+	// Data members can be public, private or protected
+	// members are trailing with _
+	string name_;
+	unsigned age_ = 22;
+public:
+	int i;
+	unsigned age() const // getter accesser
+	{
+		return age_;
+	}
+
+	void set_age(unsigned a) //setter or mutator
+	{
+		age_ = a;
+	}
+};
+
+int main()
+{
+	Person_t pt;
+	cout << pt.age << endl;
+
+	Person p;
+	cout << p.age() << endl;
+	p.set_age(24);
+	cout << p.age() << endl;
+    return 0;
+}
+
