dd5d6c94017d017ec0e882934c28f1e6357d5337
Ankit Mishra <ankit1703@gmail.com>
2017-05-22 12:23:53 +0530

    Initial Commit

---
+++ StreamIO/StreamIO.cpp
@@ -0,0 +1,13 @@
+// StreamIO.cpp 
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Standard Output library .... Hello! " << endl;
+	cout << 2 + 2 << endl;
+    return 0;
+}
+
