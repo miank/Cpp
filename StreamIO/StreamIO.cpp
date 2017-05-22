738988f7bb031ae1216697398352b8ba7f74927e
Ankit Mishra <ankit1703@gmail.com>
2017-05-22 16:45:40 +0530

    Code added

--- StreamIO/StreamIO.cpp
+++ StreamIO/StreamIO.cpp
@@ -9,7 +9,7 @@ int main()
 	cout << "Standard Output library .... Hello! " << endl;
 	cout << 2 + 2 << endl;
 
-	cout << "Test" << endl << "2 + 2" << 4 + 7 << endl;
+	cout << "Test" << endl << "2 + 2" << 4 + 7 << endl << 1.7 << endl << 1.7 + 2.4 << endl;
     return 0;
 }
 
