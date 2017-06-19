b47428fd55e57620d7d865674ae3e27aeb6abb33
Ankit Mishra <ankit1703@gmail.com>
2017-06-19 11:00:00 +0100

    Initial Commit

--- FlowControl/If.cpp
+++ FlowControl/If.cpp
@@ -23,6 +23,33 @@ int main()
 
 	if (x + y > 10)
 		cout << "thanks for choosing large numbers!" << endl;
+
+	////////////////// While Loop ////////////////////////
+	int x1;
+	cout << "Enter a number " << endl;
+	cin >> x1;
+
+	bool prime = true;
+	int i = 2;
+
+	while (i <= x1/i)
+	{
+		int factor = x1 / i;
+		if (factor * i == x1)
+		{
+			cout << "factor found: " << factor << endl;
+			prime = false;
+			break;
+		}
+		i = i + 1;
+	}
+	cout << x1 << " is " ;
+	if (prime)
+		cout << "prime " << endl;
+	else
+		cout << "not prime " << endl;
+
+
     return 0;
 }
 
