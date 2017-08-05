d052a141c964d9adb58509084532585a57ef3e37
Ankit Mishra <ankit1703@gmail.com>
2017-08-05 14:07:10 +0530

    Console App

---
+++ SimpleApplicationCpp/SimpleApplicationCpp.cpp
@@ -0,0 +1,16 @@
+// SimpleApplicationCpp.cpp 
+
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	cout << "Type your name " << endl;
+	string name;
+	cin >> name;
+	cout << "Hello, " << name << endl;
+    return 0;
+}
+
