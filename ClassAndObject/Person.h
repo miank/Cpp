796fad4f3c87e20547d58c80c3965b7d8357a04b
Ankit Mishra <ankit1703@gmail.com>
2017-08-11 05:35:47 +0530

    Initial commit

---
+++ ClassAndObject/Person.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <string>
+
+class Person
+{
+private:
+	std::string firstName;
+	std::string lastName;
+	int arbitaryNumber;
+
+public:
+	Person(std::string fName, std::string lName, int arbitary);
+
+
+
+
+};
