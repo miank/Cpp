965a816fe68fb60fb9213941323eee64a5de7ef4
Ankit Mishra <ankit1703@gmail.com>
2017-08-13 20:04:26 +0530

    All ifendif gurads

--- ClassAndObject/Person.h
+++ ClassAndObject/Person.h
@@ -2,6 +2,8 @@
 #include <string>
 using namespace std;
 
+#ifndef _PERSON_H
+#define _PERSON_H
 class Person
 {
 private:
@@ -12,3 +14,5 @@ public:
 	Person(string fName, string lName, int arbitary);
 	~Person();
 };
+
+#endif // !_PERSON_H
